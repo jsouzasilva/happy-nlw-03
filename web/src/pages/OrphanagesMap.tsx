abe5a7bbc8dbbe5d7f43b46e99be42751c5d1960
Juliano Souza <souzza.s1990@gmail.com>
2020-10-13 15:11:46 -0300

    configuring the map with leaflet.

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import {FiPlus} from 'react-icons/fi';
+import { Map, TileLayer} from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 import '../styles/pages/orphanages-map.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
@@ -20,11 +23,19 @@ function OrphanagesMap() {
           <span>Goiás</span>
         </footer>
       </aside>
-      <div>
+      
+      <Map 
+      center={[-15.7531375,-48.2891368]}
+      zoom={15}
+      style={{ width:'100%', height:'100%' }}>
+        {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>*/}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+      </Map>
+      
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF"/>
       </Link>
-      </div>
+      
     </div>
   )
 }
