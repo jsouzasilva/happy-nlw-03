4810e544ab749b23ff1e5c062a588d3c9afb453a
Juliano Souza <souzza.s1990@gmail.com>
2020-10-14 16:57:08 -0300

    abstraction of routes using controllers

--- backend/src/database/migrations/1602690148782-create_orphanages.ts
+++ backend/src/database/migrations/1602690148782-create_orphanages.ts
@@ -4,7 +4,7 @@ export class createOrphanages1602690148782 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(new Table({
-           name:'orphanage',
+           name:'orphanages',
            columns: [ 
              {
                name:  'id',
@@ -36,6 +36,10 @@ export class createOrphanages1602690148782 implements MigrationInterface {
              {
                name:  'instructions',
                type:  'text'
+             },
+             {
+               name:  'opening_hours',
+               type:  'varchar'
              },
              {
                name:  'open_on_weekends',
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get('/users', (request, response) => {
-  response.send('Olá mundo!')
-})
-
-app.listen(3333);
+app.listen(3333, () => console.log('Estamos online!'));
